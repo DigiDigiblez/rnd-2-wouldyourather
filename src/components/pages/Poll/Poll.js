0d3df7b3fa40abc9d08c7aa6d079e894d6c60f2a
Carl Bowen <carl.bowen@vibrantmedia.com>
2020-05-25 15:28:46 +0100

    fix: Fix bug (spotted by awesome Udacity reviewer) to redirect user home if they hit rogue poll id url

--- src/components/pages/Poll/Poll.js
+++ src/components/pages/Poll/Poll.js
@@ -1,6 +1,8 @@
 import "./Poll.css";
 
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Redirect, useHistory } from "react-router";
 
 import Container from "../../atoms/Container";
 import QuestionPoll from "../../molecules/QuestionPoll/QuestionPoll";
@@ -9,7 +11,17 @@ import Chrome from "../../templates/Chrome";
 const Poll = () => {
   const baseclass = "poll";
 
-  return (
+  const history = useHistory();
+
+  const [pollId] = useState(
+    history.location.pathname.replace("/questions/", "")
+  );
+
+  const [isPollIdValid] = useState(
+    useSelector(store => Boolean(store.questions[pollId]))
+  );
+
+  return isPollIdValid ? (
     <Container className={baseclass}>
       <Chrome>
         <section className={`${baseclass}__contents`}>
@@ -17,6 +29,8 @@ const Poll = () => {
         </section>
       </Chrome>
     </Container>
+  ) : (
+    <Redirect to="/404" />
   );
 };
 
